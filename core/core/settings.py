4aa3b45b65af25295297a1c2ec221b082a9f1ab0
Sdaegh Mehdizadeh <sdg.mhz@gmail.com>
2023-10-12 17:51:36 +0330

    User Custom Model
    - Post model created for app blog
    - Custom User Model created for app accounts
    - User manager class created
    - AUTH_USER_MODEL added to settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
-    'blog.apps.BlogConfig',
+    # 'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# User manager config
+AUTH_USER_MODEL = 'accounts.User'
