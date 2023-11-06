09307b9e5a9db1a4afba03b6c8b66e814abc56ac
Sdaegh Mehdizadeh <sdg.mhz@gmail.com>
2023-11-06 09:50:16 +0330

    8- CBV
    - class based views created and tested
    - TemplateView
    - RedirectView
    - ListView
    - DetailView
    - FormView
    - CreateView
    - UpdateView
    - DeletView

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User,Profile
+from .models import User, Profile
 
 
 # Register your models here.
@@ -9,20 +9,22 @@ class CustomUserAdmin(UserAdmin):
     '''
     Custom user admin for our app
     '''
-    model= User
-    list_display = ('email' , 'is_superuser' , 'is_active')
-    list_filter= ('email' , 'is_superuser' , 'is_active')
-    searching_fields=('email',)
-    ordering= ('email',)
-    fieldsets=(
+    model = User
+    list_display = ('email', 'is_superuser', 'is_active')
+    list_filter = ('email', 'is_superuser', 'is_active')
+    searching_fields = ('email',)
+    ordering = ('email',)
+    fieldsets = (
         ('Authentication', {'fields': ('email', 'password')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active','is_superuser')}),
-        ('group permissions',{'fields':('groups','user_permissions',)}),
-        ('important dates',{'fields':('last_login',)}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
+        ('group permissions', {'fields': ('groups', 'user_permissions',)}),
+        ('important dates', {'fields': ('last_login',)}),
     )
-    add_fieldsets=(
-        (None,{'classes': ('wide',),
-               'fields':('email','password1','password2','is_staff','is_active','is_superuser'),}),
+    add_fieldsets = (
+        (None, {'classes': ('wide',),
+                'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active', 'is_superuser'), }),
     )
+
+
 admin.site.register(Profile)
-admin.site.register(User,CustomUserAdmin)
+admin.site.register(User, CustomUserAdmin)
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,103 @@
-from django.shortcuts import render
-
+from typing import Any
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic import ListView, DetailView, FormView, CreateView, UpdateView, DeleteView
+from .models import Post
+from .forms import PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 # Create your views here.
+
+# Function Base View show a template
+'''
+def indexView(request):
+    """
+    a function based view to show index page
+    """
+    context = {'name': 'ali'}
+    return render(request, 'index.html', context)
+'''
+
+
+class IndexView(TemplateView):
+    '''
+    a class based view to show index page
+    '''
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['name'] = 'ali'
+        context['posts'] = Post.objects.all()
+        return context
+
+
+''' FUNCTION Base View for redirect
+from django.shortcuts import redirect
+def redirectToMaktab(request):
+    return redirect('https://maktabkhooneh.com')
+    '''
+
+
+class RedirectToMaktab(RedirectView):
+    url = 'https://maktabkhooneh.com'
+
+    def get_redirect_url(self, *args, **kwargs):
+        post = get_object_or_404(Post, pk=kwargs['pk'])
+        print(post)
+        return super().get_redirect_url(*args, **kwargs)
+
+
+class PostListView(PermissionRequiredMixin, LoginRequiredMixin, ListView):
+    permission_required = 'blog.view_post'
+
+    queryset = Post.objects.all()
+    # model = Post
+
+    context_object_name = 'posts'
+    # paginate_by = 2
+    ordering = '-id'
+
+    # def get_queryset(self):
+    #     posts = Post.objects.filter(status=True)
+    #     return posts
+
+
+class PostDetailView(LoginRequiredMixin, DetailView):
+    model = Post
+
+
+'''
+class PostCreateView(FormView):
+    template_name = 'blog/contact.html'
+    form_class = PostForm
+    success_url = '/blog/post/'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+'''
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    # fields = ['author', 'title', 'content',
+    #           'status', 'category', 'published_date']
+    form_class = PostForm
+    success_url = '/blog/post/'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostEditView(LoginRequiredMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    success_url = '/blog/post/'
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = '/blog/post/'
